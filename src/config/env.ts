c3926ed5d3afcda406651a01ae58d7abc41ff088
saikagho <saikkat7ghosh@gmail.com>
2026-06-04 20:39:32 +0200

    fix: Fix pipeline unit tests

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import dotenv from 'dotenv';
 import { z } from 'zod';
 
--- tests/setup.ts
+++ tests/setup.ts
@@ -1,3 +1,8 @@
 // Set testing environment variables
 process.env.NODE_ENV = 'test';
 process.env.PORT = '3001';
+process.env.DATABASE_HOST = process.env.DATABASE_HOST ?? 'localhost';
+process.env.DATABASE_PORT = process.env.DATABASE_PORT ?? '5432';
+process.env.DATABASE_NAME = process.env.DATABASE_NAME ?? 'test_db';
+process.env.DATABASE_USER = process.env.DATABASE_USER ?? 'test_user';
+process.env.DATABASE_PASSWORD = process.env.DATABASE_PASSWORD ?? 'test_password';
